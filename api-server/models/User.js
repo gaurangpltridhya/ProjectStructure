d2a9aea7089da07e04dda8eeaf321d75bebef203
ashishkuswah <ashishktridhyatech@gmail.com>
2022-06-06 16:38:17 +0530

    user model change

--- api-server/models/User.js
+++ api-server/models/User.js
@@ -1,24 +1,36 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-    email : {
-        type: 'string',
-        unique: true,
-        required: true,
-        lowercase: true,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: "string",
+      required: true,
     },
-    password : {
-        type: 'string',
-        required: true,
+    lastName: {
+      type: "string",
+      required: true,
     },
-    isAdmin :{
-        type: 'boolean',
-        default: false
-    }, 
-},{timestamps: true })
+    contact: {
+      type: "string",
+      required: true,
+    },
+    email: {
+      type: "string",
+      unique: true,
+      required: true,
+      lowercase: true,
+    },
+    password: {
+      type: "string",
+      required: true,
+    },
+    role: {
+      type: "string",
+      default: "user",
+    },
+  },
+  { timestamps: true }
+);
 
-const User =  mongoose.model('User',userSchema)
+const User = mongoose.model("User", userSchema);
 module.exports = User;
-
-
- 
