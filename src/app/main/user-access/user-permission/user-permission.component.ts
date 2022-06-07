8f3b82d8e2d6d0f7cdf58e13c00711b2368e1771
nishitaodedara1112 <nishitaotridhyatech@gmail.com>
2022-06-07 10:59:58 +0530

    Select All checkbox

--- src/app/main/user-access/user-permission/user-permission.component.ts
+++ src/app/main/user-access/user-permission/user-permission.component.ts
@@ -1,18 +1,11 @@
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import {ThemePalette} from '@angular/material/core';
-interface Food {
-  value: string;
-  viewValue: string;
-}
 
 
-export interface Task {
-  name: string;
-  completed: boolean;
-  color: ThemePalette;
-  subtasks?: Task[];
-}
+
+
 
 
 @Component({
@@ -22,51 +15,35 @@ export interface Task {
 })
 export class UserPermissionComponent implements OnInit {
 
-myForm: FormGroup;
+userPermissionForm: FormGroup;
 
-  foods: Food[] = [
-    {value: 'admin-0', viewValue: 'admin'},
-    {value: 'user-1', viewValue: 'user'},
-    {value: 'manager-2', viewValue: 'manager'},
-  ];
-  task: Task = {
-    name: 'Indeterminate',
-    completed: false,
-    color: 'primary',
-    subtasks: [
-      {name: 'Primary', completed: false, color: 'primary'},
-      {name: 'Accent', completed: false, color: 'accent'},
-      {name: 'Warn', completed: false, color: 'warn'},
-    ],
-  };
-  allComplete: boolean = false;
-  updateAllComplete() {
-    this.allComplete = this.task.subtasks != null && this.task.subtasks.every(t => t.completed);
-  }
-  someComplete(): boolean {
-    if (this.task.subtasks == null) {
-      return false;
-    }
-    return this.task.subtasks.filter(t => t.completed).length > 0 && !this.allComplete;
-  }
-  setAll(completed:boolean) {
-    this.allComplete = completed;
-    if(this.task.subtasks == null) {
-      return;
-    }
-    this.task.subtasks.forEach(t=> (t.completed = completed));
-  }
-  constructor(private fb: FormBuilder) { }
+
+
+  constructor(private fb: FormBuilder, private activatedRoute: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
-    // this.myForm = this.fb.group({
-    //   role: [''],
-    //   create: [''],
-    //   edit: [''],
-    //   delete: ['']
+    this.userPermissionForm = this.fb.group({
+      role: [''],
+      create: [''],
+      edit: [''],
+      delete: ['']
+    })
+  }
 
-    // })
-    // this.myForm.valueChanges.subscribe(console.log)
+  checks=false;
+  bulk(e:any ){
+if (e.target.checked==true){
+  this.checks=true;
+}
+else{
+  this.checks=false;
+
+}
+  }
+
+  onSubmit(){
+    console.log('hi', this.userPermissionForm.value);
+    this.router.navigate(['/user-access'])
   }
 
 }
