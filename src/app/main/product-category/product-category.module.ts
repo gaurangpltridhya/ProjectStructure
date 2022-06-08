a85a37707e925234488e4f324e55ef4e2a1f0a2d
gaurangpltridhya <--local>
2022-06-08 11:17:50 +0530

    add product category page added

--- src/app/main/product-category/product-category.module.ts
+++ src/app/main/product-category/product-category.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { ProductCategoryRoutingModule } from './product-category-routing.module';
 import { ProductCategoryListComponent } from './product-category-list/product-category-list.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { AddProductCategoryComponent } from './add-product-category/add-product-category.component';
 
 
 
 @NgModule({
   declarations: [
-    ProductCategoryListComponent
+    ProductCategoryListComponent,
+    AddProductCategoryComponent
   ],
   imports: [
     CommonModule,
